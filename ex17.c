4fca3f1bc1372e611a433a2ddf12b41ca2aecb39
Ricardo Henrique Simplicio Dimas <rhenrique.dimas@gmail.com>
2016-10-28 16:23:33 -0300

    add arquivo .c

---
+++ ex17.c
@@ -0,0 +1,36 @@
+/* ************************************************************************ *
+ *    Calculadora Polonesa, VERSION 0.1                                     *
+ *    Calcula uma equacao na notacao polonesa usando pilha                  *
+ *                                                                          *
+ *    Copyright (C) 2015 by Ricardo Henrique Simplicio Dimas                *
+ *                                                                          *
+ *    This program is free software; you can redistribute it and/or modify  *
+ *    it under the terms of the GNU General Public License as published by  *
+ *    the Free Software Foundation; either version 2 of the License, or     *
+ *    (at your option) any later version.                                   *
+ *                                                                          *
+ *    This program is distributed in the hope that it will be useful,       *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *    GNU General Public License for more details.                          *
+ *                                                                          *
+ *    You should have received a copy of the GNU General Public License     *
+ *    along with this program; if not, write to the                         *
+ *    Free Software Foundation, Inc.,                                       *
+ *    59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ *                                                                          *
+ *    To contact the author, please write to:                               *
+ *    Ricardo Henrique Simplicio Dimas <rhenrique.dimas@gmail.com>          *
+ *    Webpage: http://www.upe.br                                            *
+ *    Phone: +55 (81) 99730-4919                                            *
+ * ************************************************************************ *
+ * 
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv)
+{
+    return 0;
+}
+
